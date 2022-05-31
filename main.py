2e4bc3e74a8e77522e21b16fa1043779362e0462
Thib <64400734+Thibaut-HERTZOG@users.noreply.github.com>
2022-05-31 22:17:30 +0200

    Strat 2 : graph OK

--- main.py
+++ main.py
@@ -49,39 +49,45 @@ plt.show()
 #strategie 2
 pkn = [0]*K
 maxpkn = 0
-listePatients = [0]*K
-listeSurvecus = [0]*K
-courbe0 = []; courbe1 = []; courbe2 = []; courbe3 = []; courbe4 = []
-courbe5 = []; courbe6 = []; courbe7 = []; courbe8 = []; courbe9 = []
-moyCourbe0 = []; moyCourbe1 = []; moyCourbe2 = []; moyCourbe3 = []; moyCourbe4 = []
-moyCourbe5 = []; moyCourbe6 = []; moyCourbe7 = []; moyCourbe8 = []; moyCourbe9 = []
+courbe0 = np.zeros(N); courbe1 = np.zeros(N); courbe2 = np.zeros(N); courbe3 = np.zeros(N);
+courbe4 = np.zeros(N); courbe5 = np.zeros(N); courbe6 = np.zeros(N); courbe7 = np.zeros(N);
+courbe8 = np.zeros(N); courbe9 = np.zeros(N)
 
 def strat2init(n):
     listePatients[n] += 1
     pkn[n] = np.random.binomial(K, probaK[n])
     if(np.random.binomial(1, probaK[n]) == 1):
         listeSurvecus[n] += 1
+    courbe0[0] = 0
+    courbe1[0] = 0
+    courbe2[0] = 0
+    courbe3[0] = 0
+    courbe4[0] = 0
+    courbe5[0] = 0
+    courbe6[0] = 0
+    courbe7[0] = 0
+    courbe8[0] = 0
+    courbe9[0] = 0
 
-def strat2choix(n):
+def strat2choix(n,N):
     listePatients[n] += 1
     pkn[n] = np.random.binomial(N, probaK[n])/listePatients[n]
     if(np.random.binomial(1, probaK[n]) == 1):
         listeSurvecus[n] += 1
-    addPts()
+    courbe0[N] += listeSurvecus[0]
+    courbe1[N] += listeSurvecus[1]
+    courbe2[N] += listeSurvecus[2]
+    courbe3[N] += listeSurvecus[3]
+    courbe4[N] += listeSurvecus[4]
+    courbe5[N] += listeSurvecus[5]
+    courbe6[N] += listeSurvecus[6]
+    courbe7[N] += listeSurvecus[7]
+    courbe8[N] += listeSurvecus[8]
+    courbe9[N] += listeSurvecus[9]
 
-def addPts():
-    courbe0.append(listeSurvecus[0])
-    courbe1.append(listeSurvecus[1])
-    courbe2.append(listeSurvecus[2])
-    courbe3.append(listeSurvecus[3])
-    courbe4.append(listeSurvecus[4])
-    courbe5.append(listeSurvecus[5])
-    courbe6.append(listeSurvecus[6])
-    courbe7.append(listeSurvecus[7])
-    courbe8.append(listeSurvecus[8])
-    courbe9.append(listeSurvecus[9])
-
-for rep in range(1):
+for rep in range(10):
+    listePatients = [0] * K
+    listeSurvecus = [0] * K
     #initialisation
     for j in range(K):
         strat2init(j)
@@ -93,27 +99,31 @@ for rep in range(1):
     #initialisation fin
 
     for j in range(K,N):
-        strat2choix(maxpkn)
+        strat2choix(maxpkn,j)
 
         for i in range(K):
             if pkn[maxpkn] < pkn[i]:
                 maxpkn = i
 
-print(pkn)
+plt.plot(courbe0/10,label='Remède 1')
+plt.plot(courbe1/10,label='Remède 2')
+plt.plot(courbe2/10,label='Remède 3')
+plt.plot(courbe3/10,label='Remède 4')
+plt.plot(courbe4/10,label='Remède 5')
+plt.plot(courbe5/10,label='Remède 6')
+plt.plot(courbe6/10,label='Remède 7')
+plt.plot(courbe7/10,label='Remède 8')
+plt.plot(courbe8/10,label='Remède 9')
+plt.plot(courbe9/10,label='Remède 10')
+plt.title('Evolution du nombre de patients guéris en \n fonction du nombre de patients testés')
+plt.xlabel('Somme des patients testés')
+plt.ylabel('Nombre de patients guéris')
+plt.grid()
+plt.legend()
+plt.show()
+
 print(listePatients)
 print(listeSurvecus)
-
-plt.plot(courbe0)
-plt.plot(courbe1)
-plt.plot(courbe2)
-plt.plot(courbe3)
-plt.plot(courbe4)
-plt.plot(courbe5)
-plt.plot(courbe6)
-plt.plot(courbe7)
-plt.plot(courbe8)
-plt.plot(courbe9)
-plt.show()
 
 #strategie 3
 pkn = [0]*K
@@ -170,10 +180,10 @@ for rep in range(1):
             if bornSupPkn[maxpkn] < bornSupPkn[i]:
                 maxpkn = i
 
-print(pkn)
-print(bornSupPkn)
-print(listePatients)
-print(listeSurvecus)
+#print(pkn)
+#print(bornSupPkn)
+#print(listePatients)
+#print(listeSurvecus)
 
 plt.plot(courbe0)
 plt.plot(courbe1)
