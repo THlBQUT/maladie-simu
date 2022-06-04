7d3af0e78d3e83bb47883a7ce223f64e43c98cde
jerem <jeremy-2000@hotmail.fr>
2022-06-04 18:32:35 +0200

    Strat2

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random as rd
 import matplotlib.pyplot as plt
+from scipy.stats import beta
 
 def addToListStrat1(n):
     listePatients[n] += 1
@@ -248,3 +249,95 @@ plt.legend()
 ax.legend(loc='center right')
 plt.show()
 
+#strategie 4
+pkn = [0]*K
+maxpkn = 0
+# listes pour chaque remèdes avec N colonnes pour constater l'évolution du nbr de personnes guéris
+courbe0 = np.zeros(N); courbe1 = np.zeros(N); courbe2 = np.zeros(N); courbe3 = np.zeros(N);
+courbe4 = np.zeros(N); courbe5 = np.zeros(N); courbe6 = np.zeros(N); courbe7 = np.zeros(N);
+courbe8 = np.zeros(N); courbe9 = np.zeros(N)
+
+# listes pour chaque remèdes avec N colonnes pour constater l'évolution du nbr de pers ayant eu le remede X
+NbPatient0 = np.zeros(N); NbPatient1 = np.zeros(N); NbPatient2 = np.zeros(N); NbPatient3 = np.zeros(N);
+NbPatient4 = np.zeros(N); NbPatient5 = np.zeros(N); NbPatient6 = np.zeros(N); NbPatient7 = np.zeros(N);
+NbPatient8 = np.zeros(N); NbPatient9 = np.zeros(N)
+
+def strat4init(n):
+    listePatients[n] += 1
+    pkn[n] = beta(1,1).rvs()
+    if(np.random.binomial(1, probaK[n]) == 1):
+        listeSurvecus[n] += 1
+    courbe0[0] , courbe1[0], courbe2[0], courbe3[0], courbe4[0] = 0, 0, 0, 0, 0
+    courbe5[0] , courbe6[0], courbe7[0], courbe8[0], courbe9[0] = 0, 0, 0, 0, 0
+
+def strat4choix(n,N):
+    listePatients[n] += 1
+    pkn[n] = beta(1+listeSurvecus[n],1+ listePatients[n] - listeSurvecus[n]).rvs()
+    if(np.random.binomial(1, probaK[n]) == 1):
+        listeSurvecus[n] += 1
+    #Incrémentation la liste des personnes guéris dans la liste de chaque remèdes N étant le numéro du patient
+    courbe0[N] += listeSurvecus[0]; courbe1[N] += listeSurvecus[1]; courbe2[N] += listeSurvecus[2]
+    courbe3[N] += listeSurvecus[3]; courbe4[N] += listeSurvecus[4]; courbe5[N] += listeSurvecus[5]
+    courbe6[N] += listeSurvecus[6]; courbe7[N] += listeSurvecus[7]; courbe8[N] += listeSurvecus[8]
+    courbe9[N] += listeSurvecus[9]
+    NbPatient0[N] += listeSurvecus[0] / listePatients[0]; NbPatient1[N] += listeSurvecus[1] / listePatients[1]
+    NbPatient2[N] += listeSurvecus[2] / listePatients[2]; NbPatient3[N] += listeSurvecus[3] / listePatients[3]
+    NbPatient4[N] += listeSurvecus[4] / listePatients[4]; NbPatient5[N] += listeSurvecus[5] / listePatients[5]
+    NbPatient6[N] += listeSurvecus[6] / listePatients[6]; NbPatient7[N] += listeSurvecus[7] / listePatients[7]
+    NbPatient8[N] += listeSurvecus[8] / listePatients[8]; NbPatient9[N] += listeSurvecus[9] / listePatients[9]
+
+
+for rep in range(10):
+    listePatients = [0] * K
+    listeSurvecus = [0] * K
+    #initialisation
+    for j in range(K):
+        strat4init(j)
+
+    for j in range(K):
+        if pkn[maxpkn] < pkn[j]:
+            maxpkn = j
+
+    #initialisation fin
+
+    for j in range(K,N):
+        strat4choix(maxpkn,j)
+
+        for i in range(K):
+            if pkn[maxpkn] < pkn[i]:
+                maxpkn = i
+plt.plot(courbe0/10,label='Remède 1')
+plt.plot(courbe1/10,label='Remède 2')
+plt.plot(courbe2/10,label='Remède 3')
+plt.plot(courbe3/10,label='Remède 4')
+plt.plot(courbe4/10,label='Remède 5')
+plt.plot(courbe5/10,label='Remède 6')
+plt.plot(courbe6/10,label='Remède 7')
+plt.plot(courbe7/10,label='Remède 8')
+plt.plot(courbe8/10,label='Remède 9')
+plt.plot(courbe9/10,label='Remède 10')
+plt.title('Evolution du nombre de patients guéris en \n fonction du nombre de patients testés')
+plt.xlabel('Somme des patients testés')
+plt.ylabel('Nombre de patients guéris')
+plt.grid()
+plt.legend()
+plt.show()
+
+ax = plt.subplot(111)
+plt.plot(NbPatient0,label='Remède 1')
+plt.plot(NbPatient1,label='Remède 2')
+plt.plot(NbPatient2,label='Remède 3')
+plt.plot(NbPatient3,label='Remède 4')
+plt.plot(NbPatient4,label='Remède 5')
+plt.plot(NbPatient5,label='Remède 6')
+plt.plot(NbPatient6,label='Remède 7')
+plt.plot(NbPatient7,label='Remède 8')
+plt.plot(NbPatient8,label='Remède 9')
+plt.plot(NbPatient9,label='Remède 10')
+plt.title('Pourcentage de patients guéris en \n fonction du nombre de patients testés')
+plt.xlabel('Somme des patients testés')
+plt.ylabel('Pourcentage de patients guéris')
+plt.grid()
+plt.legend()
+ax.legend(loc='center right')
+plt.show()
